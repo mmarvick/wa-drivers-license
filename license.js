311a689e4a62060d79d1d2a869e1bc5c4fd12901
Michael Marvick <mmarvick@gmail.com>
2014-04-24 23:45:56 -0700

    Added warnings and heading

--- license.js
+++ license.js
@@ -7,6 +7,7 @@ var alt_month_out = ["S", "T", "U", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var day_out =   	["A", "B", "C", "D", "E", "F", "G", "H", "Z", "S", "J", "K", "L", "M", "N", "W", "P", "Q", "R", "O", "1", "2", "3", "4", "5", "2", "7", "8", "9", "T", "U"]
 
 function calculate (form) {
+	var errors = [];
 	var year = parseInt(form.year.value);
 	var month = parseInt(form.month.value);
 	var day = parseInt(form.day.value);
@@ -14,9 +15,9 @@ function calculate (form) {
 	var middle = formatname(form.middlename.value, 1);
 	var last = formatname(form.lastname.value, 5);
 
-	var monthcode = getmonthcode(month);
-	var daycode = getdaycode(day);
-	var yearcode = getyearcode(year);
+	var monthcode = getmonthcode(month, errors);
+	var daycode = getdaycode(day, errors);
+	var yearcode = getyearcode(year, errors);
 	var check = getcheck(yearcode, monthcode, daycode, first, middle, last);
 
 	var altmonthcode = getaltmonthcode(month);
@@ -25,15 +26,32 @@ function calculate (form) {
 	var license = last+first+middle+yearcode+check+monthcode+daycode;
 	var altlicense = last+first+middle+yearcode+altcheck+altmonthcode+daycode;
 
+	var process
+
 	document.getElementById("form-col").className="eq-height col-sm-6";
-	document.getElementById("info-col").style.display="inline";
-	document.getElementById("info").innerHTML = generateHTML(license, altlicense);
+	document.getElementById("info-col").style.display = "inline";
+	document.getElementById("panel-right").style.height = document.getElementById("panel-left").clientHeight + 'px';
+	if (errors.length > 0) {
+		displayErrors(errors);
+	} else {
+		generateHTML(license, altlicense);
+	}
 };
 
 function generateHTML(license, altlicense) {
+	document.getElementById("panel-right").className="panel panel-primary";
 	html = "<h4>License Number:</h4>" + license + "<br /><br /><h4>Alternate License Number:</h4>" + altlicense;
-	//html = license;
-	return html;
+	document.getElementById("info").innerHTML = html;
+}
+
+function displayErrors(errors) {
+	document.getElementById("panel-right").className="panel panel-danger";
+	html = "<h4>Errors:</h4><ul>";
+	for (var i=0; i<errors.length; i++) {
+		html += "<li>" + errors[i] + "</li>";
+	}
+	html += "</ul>";
+	document.getElementById("info").innerHTML = html;
 }
 
 function formatname(value, length) {
@@ -56,7 +74,11 @@ function getcheck(yearcode, monthcode, daycode, first, middle, last) {
 	return ((check % 10)+10)%10;
 }
 
-function getyearcode(year) {
+function getyearcode(year, errors) {
+	if (isNaN(year)) {
+		errors.push("Invalid year");
+		return;
+	}
 	year = 100 - year%100;
 	if (year == 100) {
 		return "00"
@@ -82,7 +104,11 @@ function getaltmonthcode(month) {
 	return alt_month_out[month-1];
 };
 
-function getdaycode(day) {
+function getdaycode(day, errors) {
+	if (isNaN(day) || day < 1 || day > 31) {
+		errors.push("Invalid day");
+		return;
+	}
 	return day_out[day-1];
 };
 
